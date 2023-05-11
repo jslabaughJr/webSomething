e483c2305dcb0b45febdc61bbc23d576c2cdfc83
johnny <jslabauugh@gmail.com>
2023-05-11 12:39:13 -0400

    Initial Commit

---
+++ webSomething/data.swift
@@ -0,0 +1,8 @@
+//
+//  data.swift
+//  webSomething
+//
+//  Created by Johnny Slabaugh on 4/5/23.
+//
+
+import Foundation
---
+++ webSomething/prefrencesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  prefrencesViewController.swift
+//  webSomething
+//
+//  Created by Johnny Slabaugh on 4/5/23.
+//
+
+import Foundation
